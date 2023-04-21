abd91589c2d0b8db2d1c0eb837385c6e22bfe34f
sylvain-huppe-infosh <sylvain.huppe@infosh.ca>
2023-04-21 13:49:14 -0400

    Ajout des Auteurs au web

---
+++ src/main/java/guru/springframework/spring6webapp/services/AuthorService.java
@@ -0,0 +1,12 @@
+package guru.springframework.spring6webapp.services;
+
+import guru.springframework.spring6webapp.domain.Author;
+
+/**
+ * Created by jt, Spring Framework Guru.
+ */
+public interface AuthorService {
+
+    Iterable<Author> findAll();
+
+}
